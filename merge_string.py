0ce27fe3f1d40042a90892263ec5929e02e46cdb
MotteyAmos <101886946+MotteyAmos@users.noreply.github.com>
2023-05-27 20:34:57 +0000

    Create merge_string.py

---
+++ merge_string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merge_string = ""
+        i = 0
+        while i < len(word1) and i < len(word2):
+
+            merge_string += word1[i] + word2[i]
+            i += 1
+        
+        if (len(word1) > len(word2)):
+            merge_string += word1[i:]
+        else:
+            merge_string += word2[i:]
+        
+        return merge_string
